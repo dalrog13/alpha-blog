37c5454115fe3004d647b4f35da0c15db5f20714
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2021-04-28 23:42:29 -0700

    Create article flow and error messaging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to (@article)
+        else
+            render 'new'
+        end
+    end
 end
