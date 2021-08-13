013ff287ac7765413f8cd0fb1d4acfd525b3bba3
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2021-08-12 20:43:31 -0700

    Add password column to users, and secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }, 
                         presence: true, length: {maximum: 105}, 
                         format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
