bf648e6dce72d6002ddc883f878ac51e83eadce7
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2021-08-26 13:57:38 -0700

    Layout updates on the user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,39 +3,43 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       @articles = @user.articles
     end
+
+    def index
+      @users = User.all
+    end
         
     def new
-        @user = User.new
+      @user = User.new
     end
 
     def edit
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
         if @user.update(user_params)
           flash[:notice] = "Your account information was successfully updated"
           redirect_to articles_path
 
         else
           render 'edit'
-        end
+      end
     end
 
     def create
-        @user = User.new(user_params)
+      @user = User.new(user_params)
         if @user.save
           flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
           redirect_to articles_path
         else
           render 'new'
-        end
+      end
     end
     
-      private
-      def user_params
+    private
+    def user_params
         params.require(:user).permit(:username, :email, :password)
-      end
+    end
     
 end
