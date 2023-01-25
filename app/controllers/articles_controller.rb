8be666818b7eb983b1ddffb8b751c4d21bf768ee
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2023-01-24 21:35:21 -0800

    restrict actions for articles controller (create/delete)
    Hard prevent users from editing/ deleting other user's articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def show
 
     end
@@ -50,5 +52,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only modify your own article"
+        redirect_to @article
+      end
     end
 end
