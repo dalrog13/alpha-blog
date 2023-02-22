19658225fb97a8ed5d58bb319c51abfe9ee72483
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2023-02-21 21:58:10 -0800

    add admin actions for admin users
    allow admins to edit and delete all articles and users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
     def destroy
       @user.destroy
-      session[:user_id] = nil
+      session[:user_id] = nil if @user == current_user
       flash[:notice] = "account has been successfully deleted"
       redirect_to articles_path
     end
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:alert] = "You can only modify your own account"
         redirect_to @user
       end
