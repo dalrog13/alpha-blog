cb5c53c6e153caa58a1d308afd21fa793b396852
dalrog13 <57974653+dalrog13@users.noreply.github.com>
2023-03-10 20:10:04 -0800

    create category model and tests

---
+++ test/models/category_test.rb
@@ -0,0 +1,8 @@
+class CategoryTest < ActiveSupport::TestCase
+
+    test "category should be valid" do
+        @category = Category.new(name: "Sports")
+        assert @category.valid?
+    end
+
+end
